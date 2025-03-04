3118911e958b030a46f1747ec109008e435604d1
TikeshKardate <tikesh.kardate@gmail.com>
2025-03-04 12:27:14 +0530

    [Tikesh] Add. Email regex JS problems added

---
+++ Email_Problems/Email_UC4.js
@@ -0,0 +1,28 @@
+/*
+Email UC 4
+Lets handle optional part i.e. xyz in abc.xyz@bridgelabz.co.in
+NOTE: make sure only following are valid special characters _,+, -,.
+proceeding to xyz 
+*/
+
+
+function isValidEmail(email) {
+    return /^abc([._+\-][a-zA-Z0-9]+)?@bridgelabz\.co(\.[a-zA-Z]{2,})?$/.test(email);
+}
+
+function testCases(email) {
+    if (isValidEmail(email)) {
+        console.log("Valid Email");
+    } else {
+        console.log("Invalid Email");
+    }
+}
+
+let email1 = "abc@bridgelabz.co";
+testCases(email1);
+
+let email2 = "abc.xyz@bridgelabz.co.in";
+testCases(email2);
+
+let email3 = "abc@bridgelabz"; 
+testCases(email3);
